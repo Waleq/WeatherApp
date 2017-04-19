9b873fafeef66f8003a2570d1a9d28b4cda6e42d
Mateusz Wala <mateusz.wala@hycom.pl>
2017-04-19 16:21:20 +0200

    Changes in activity

--- app/src/main/java/pro/waleq/test/MainActivity.java
+++ app/src/main/java/pro/waleq/test/MainActivity.java
@@ -2,12 +2,48 @@ package pro.waleq.test;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import retrofit2.Retrofit;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final static String APIURL = "http://api.openweathermap.org/data/2.5/weather?q=Lodz,pl&appid=";
+
+    private final static String APIKEY = "924bd4cc4333712a5cec7b4953fc510d";
+
+    private TextView deegresView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        deegresView = (TextView) findViewById(R.id.deegres);
+        refresh();
     }
+
+    protected void forceRefresh() {
+        refresh();
+    }
+
+    private void refresh() {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(APIURL + APIKEY)
+                .build();
+        Map<String, Object> service = retrofit.create(HashMap.class);
+        if (service != null) {
+            Map<String, Object> main = (HashMap<String, Object>) service.get("main");
+            if (main != null) {
+                Double temp = (Double) main.get("temp");
+                if (temp != null) {
+                    deegresView.setText(temp.toString());
+                    deegresView.invalidate();
+                }
+            }
+        }
+    }
+
 }
